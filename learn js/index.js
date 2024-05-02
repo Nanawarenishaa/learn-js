49f68707d1f6792d914e8351304798d43526f175
Nanawarenishaa <164838299+Nanawarenishaa@users.noreply.github.com>
2024-05-02 21:20:50 +0530

    Add files via upload

--- learn js/index.js
+++ learn js/index.js
@@ -1,1 +1,24 @@
+
+             // DAY-1
+
+
 console.log("hello");
+
+//lets see about variables.
+const MyId=12001
+let MyEmail="nisha123@gmail.com";
+var MyPassword="1234";
+MyCity="Nagar";
+let MyState;
+
+// MyId = 23  // not allowed
+
+MyEmail = "nanaware@gmail.com"
+MyPassword = "4321"
+MyCity = "satara"
+
+/*console.log(MyEmail);
+console.log(MyCity);
+console.log(MyPassword);*/
+
+console.table([MyId,MyEmail,MyPassword,MyCity,MyState]);
