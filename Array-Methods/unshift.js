6de1312f109f800e37ea6a6f6037f42775276658
Nanawarenishaa <164838299+Nanawarenishaa@users.noreply.github.com>
2024-06-25 21:37:29 +0530

    Add files via upload

---
+++ Array-Methods/unshift.js
@@ -0,0 +1,20 @@
+
+
+// 1.
+// let num = [ 4,5,6 ];
+// num.unshift(1,2,3 );
+// console.log(num);
+ 
+// 2.
+// let counts = [];
+// for (let i = 5 ; i >= 1 ; i--) {
+//     counts.unshift(i);   
+// }
+// console.log(counts);
+
+// 3.
+let arr = [ [ 3, 4 ] , [ 5, 6 ] ,[ 1, 2 ] ] ;
+let arr2 = arr.pop();
+arr.unshift(arr2);
+console.log(arr);
+
