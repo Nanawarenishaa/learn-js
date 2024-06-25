289b8622ecff53b6e0879769ed23c8452c30259c
Nanawarenishaa <164838299+Nanawarenishaa@users.noreply.github.com>
2024-06-25 21:05:40 +0530

    Add files via upload

--- Array-Methods/Push&Pop.js
+++ Array-Methods/Push&Pop.js
@@ -1,11 +1,35 @@
-let array =[ 67 , 78 ,45 , 23 , 91];
-array.pop();    // delete 91
-console.log(array.pop());
-array.push(13);   // add 13 at last pos
-console.log(array.push(34));   // add 34 at last pos and with console it gives us index of that element - 6
-console.log(array);   //[ 67, 78, 45, 23, 13, 34 ]
+// let array =[ 67 , 78 ,45 , 23 , 91];
+// array.pop();    // delete 91
+// console.log(array.pop());
+// array.push(13);   // add 13 at last pos
+// console.log(array.push(34));   // add 34 at last pos and with console it gives us index of that element - 6
+// console.log(array);   //[ 67, 78, 45, 23, 13, 34 ]
 
 // 1.
-let num = [ 1, 2, 3, 4 ];
-num.push( 5 , 6 , 7);
-console.log(num);
+// let num = [ 1, 2, 3, 4 ];
+// num.push( 5 , 6 , 7);
+// console.log(num);
+
+// 2.
+// let num2=[];
+// for(let i = 1; i <=10 ; i++){
+//  num2.push(i);
+// }
+// let copyName=num2;
+// copyName.pop();
+// console.log(num2);
+// console.log(copyName);
+
+
+// 3.
+function Nogenerator(){
+    return Math.floor(Math.random()*1000);
+}
+let randomNo=[];
+for (let i = 0; i <= 6 ; i++) {
+    randomNo.push(Nogenerator());  
+}
+console.log(randomNo);
+
+
+
