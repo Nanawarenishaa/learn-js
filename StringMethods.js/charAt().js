e0faeba207679baa5a727d9eab8d82e258895313
Nanawarenishaa <164838299+Nanawarenishaa@users.noreply.github.com>
2024-06-25 20:25:58 +0530

    Add files via upload

--- StringMethods.js/charAt().js
+++ StringMethods.js/charAt().js
@@ -3,3 +3,4 @@
 
 let name = new String( "Nisha Nanaware" );
 console.log(name.charAt(6));
+console.log(name[4]);
---
+++ StringMethods.js/substring().js
@@ -0,0 +1,6 @@
+
+
+
+
+let name = new String( "Nisha Nanaware" );
+console.log(name.substring(0,4));
