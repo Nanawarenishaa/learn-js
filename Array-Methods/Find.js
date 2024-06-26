df102a4651717ef1d43cb926a3c32c1e79c7d4cf
Nanawarenishaa <164838299+Nanawarenishaa@users.noreply.github.com>
2024-06-26 13:30:44 +0530

    Add files via upload

---
+++ Array-Methods/Find.js
@@ -0,0 +1,32 @@
+// ++++++++++++++++ Find() +++++++++++++++
+
+ // returns the value of the first ele that satisfies provided fuction
+// let arr = [ 1, 2, 3, 4 ];
+// let found = arr.find((ele) => ele < 4);
+// console.log(found);
+
+// let arr = [ 1, 2, 3, 4 ];
+// let found = arr.find((ele) => ele == 4);
+// console.log(found);
+
+// case sensitive
+// let fruits = [ 'apple' , 'banana' , 'orange' , 'grape' , 'Avocado'
+// ];
+// let found = fruits.find( ele => ele.startsWith("A"));
+// console.log(found);
+
+
+// let fruits = [ 'apple' , 'banana' , 'orange' , 'grape' , 'Avocado'
+// ];
+// let found = fruits.find( ele => ele.startsWith("o"));
+// console.log(found);
+
+let product = [
+    {name:"laptop" , price:1000},
+    {name:"microwave" , price:1200},
+    {name:"phone" , price:500},
+    {name:"Table" , price:700},
+];
+let expensive= product.find( ele => ele.price > 700);
+console.log(expensive);
+
