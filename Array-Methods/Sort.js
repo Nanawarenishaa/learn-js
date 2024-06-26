7ca9873757e5f481252821b54bf6d3a8b47ef83d
Nanawarenishaa <164838299+Nanawarenishaa@users.noreply.github.com>
2024-06-26 14:40:54 +0530

    Add files via upload

--- Array-Methods/Sort.js
+++ Array-Methods/Sort.js
@@ -8,9 +8,27 @@
 // let sortedByAsce = numbers.sort((a,b) => a-b );
 // console.log(sortedByAsce );
 
+// let product = [
+//     {name:"laptop" , price:1000},
+//     {name:"microwave" , price:1200},
+//     {name:"phone" , price:500},
+//     {name:"Table" , price:700},
+// ];
+// let sortedByAsce= product.sort( (a,b) => a.price - b.price );
+// console.log(sortedByAsce);
+
 
   // descending order
-let numbers =[ 10 , 36 , 78 , 45 , 5 , 8];
-let sortedBydesce = numbers.sort((a,b) => b-a );
-console.log(sortedBydesce );
+// let numbers =[ 10 , 36 , 78 , 45 , 5 , 8];
+// let sortedBydesce = numbers.sort((a,b) => b-a );
+// console.log(sortedBydesce );
 
+
+let product = [
+    {name:"laptop" , price:1000},
+    {name:"microwave" , price:1200},
+    {name:"phone" , price:500},
+    {name:"Table" , price:700},
+];
+let sortedBydesce= product.sort( (a,b) => b.price - a.price );
+console.log(sortedBydesce);
