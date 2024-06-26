ed2edf32922a59ef1a6373749bf3529c2c62771c
Nanawarenishaa <164838299+Nanawarenishaa@users.noreply.github.com>
2024-06-26 13:45:19 +0530

    Add files via upload

---
+++ Array-Methods/Every.js
@@ -0,0 +1,10 @@
+// +++++++++++++++ every() +++++++++++++++++
+// weather all elements in the array passes the test implemented by the provided function . it returns the 'true' otherwise 'false'.
+
+// let numbers = [ 8, 2, 6, 4 ];
+// let even = numbers.every( num => num % 2 === 0 );
+// console.log(even); 
+
+// let temp = [ 98 , 93 , 80 , 58 , 95 ];
+// let result = temp.every( ele => ele <= 99 );
+// console.log(result);
