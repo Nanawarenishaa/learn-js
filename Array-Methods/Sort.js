8565e417fa9d695242e251cc66425a5db1d69cfc
Nanawarenishaa <164838299+Nanawarenishaa@users.noreply.github.com>
2024-06-26 14:29:11 +0530

    Add files via upload

---
+++ Array-Methods/Sort.js
@@ -0,0 +1,16 @@
+//++++++++++++++ sort() ++++++++++++++++++
+
+// sorts the ele of an array and returns the sorted array.
+
+
+    // Ascending order
+// let numbers =[ 10 , 36 , 78 , 45 , 5 , 8];
+// let sortedByAsce = numbers.sort((a,b) => a-b );
+// console.log(sortedByAsce );
+
+
+  // descending order
+let numbers =[ 10 , 36 , 78 , 45 , 5 , 8];
+let sortedBydesce = numbers.sort((a,b) => b-a );
+console.log(sortedBydesce );
+
