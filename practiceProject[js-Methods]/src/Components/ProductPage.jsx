1a8cc826a169d4c868c12e25fb0ca3cf2cb45998
Nanawarenishaa <164838299+Nanawarenishaa@users.noreply.github.com>
2024-06-27 14:37:05 +0530

    Add files via upload

--- practiceProject[js-Methods]/src/Components/ProductPage.jsx
+++ practiceProject[js-Methods]/src/Components/ProductPage.jsx
@@ -3,56 +3,86 @@ import { BookData } from "./Data";
 
 const ProductPage = () => {
   const [search, setSearch] = useState("");
-  const [ Price, setPrice] = useState(1000);
+  const [genre, setGenre] = useState("");
+  const [sort, setSort] = useState("");
+  const [rating, setRating] = useState(5);
+  const [price, setPrice] = useState(1000);
 
   const filteredData = BookData.filter(book => {
     const matchesSearch = search === "" || book.title.toLowerCase().includes(search.toLowerCase());
-    const PriceRange = Price === "" || book.price <= Price ;
-    return matchesSearch && PriceRange;
+    const priceRange = price === "" || book.price <= price;
+    const genreSearch = genre === "" || book.genre.toLowerCase().includes(genre.toLowerCase());
+    const ratingRange = book.rating <= rating;
+    return matchesSearch && priceRange && genreSearch && ratingRange;
+  });
+
+  const sortedData = filteredData.sort((a, b) => {
+    if (sort === "Sort Ascending") {
+      return a.year - b.year;
+    }
+    if (sort === "Sort Descending") {
+      return b.year - a.year;
+    }
+    return 0;
   });
 
   return (
     <div className="w-full min-h-screen flex bg-gray-900 text-white">
-      <div className="w-[30%] h-screen bg-gray-800 p-4">
+      <div className=" w-[34%] sm:w-[25%]  h-screen bg-gray-800 p-4">
         <h1 className="text-2xl font-bold mb-4">Book List</h1>
         <div className="mb-4">
           <h2 className="text-xl font-bold">Filters</h2>
           <div className="mt-4 flex flex-col gap-2">
-            <span className="block mb-2">Price Range</span>
-            <label htmlFor="">{Price}</label>
+            <label className="block mb-2">Price Range</label>
+            <label htmlFor="">{price}</label>
             <input
               type="range"
               min={0}
               max={1000}
-              placeholder="Max"
-              value={Price}
+              value={price}
               onChange={(e) => setPrice(e.target.value)}
               className="block mb-4 p-2 w-full bg-gray-700 border border-gray-600 rounded"
             />
           </div>
           <div className="mb-4">
-            <span className="block mb-2">Genre</span>
+            <label className="block mb-2">Genre</label>
             <input
               type="text"
-              name="genre"
+              value={genre}
+              onChange={(e) => setGenre(e.target.value)}
               placeholder="Genre"
               className="block mb-4 p-2 w-full bg-gray-700 border border-gray-600 rounded"
             />
           </div>
           <div className="mb-4">
-            <span className="block mb-2"> Rating</span>
+            <label className="block mb-2">Rating</label>
             <input
               type="number"
-              name="Rating"
+              value={rating}
+              onChange={(e) => setRating(e.target.value)}
               placeholder="Rating"
               className="block mb-4 p-2 w-full bg-gray-700 border border-gray-600 rounded"
             />
           </div>
-          <button className="block w-full py-2 bg-cyan-500 hover:bg-cyan-500 rounded">Apply Filters</button>
+          <div className="flex flex-col gap-4">
+            <label>Sort by year:</label>
+            <button
+              className="px-4 py-2 bg-cyan-500 hover:bg-cyan-600 text-white font-bold rounded"
+              onClick={() => setSort("Sort Ascending")}
+            >
+              Sort Ascending
+            </button>
+            <button
+              onClick={() => setSort("Sort Descending")}
+              className="px-4 py-2 bg-cyan-500 hover:bg-cyan-600 text-white font-bold rounded"
+            >
+              Sort Descending
+            </button>
+          </div>
         </div>
       </div>
       <div className="container p-4 flex-grow">
-        <div className="mb-4 px-20 flex gap-2">
+        <div className="mb-4 flex gap-2">
           <input
             type="search"
             placeholder="Search by title"
@@ -60,12 +90,12 @@ const ProductPage = () => {
             onChange={(e) => setSearch(e.target.value)}
             className="p-2 w-full bg-gray-700 border border-gray-600 rounded"
           />
-          <button className="px-4 py-2 bg-cyan-500 hover:bg-cyan-500 text-white font-bold rounded">
+          <button className="px-4 py-2 bg-cyan-500 hover:bg-cyan-600 text-white font-bold rounded">
             Search
           </button>
         </div>
-        <div className="grid grid-cols-3 gap-8 p-8">
-          {filteredData.map((book, index) => (
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-8 p-4">
+          {sortedData.map((book, index) => (
             <div
               className="border border-gray-700 rounded-md p-4 bg-indigo-800 hover:bg-indigo-600 transition duration-300"
               key={index}
